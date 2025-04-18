5bf6ab88063f448ee209c7bdef434f366189418a
anmol-k14 <anmolkumar.1403@gmail.com>
2025-04-18 19:17:15 +0530

    jwt error

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -33,7 +33,7 @@ userSchema.methods.isValidPassword =async function(password){
 userSchema.methods.generateJWT=function(){
     return jwt.sign(
         {email:this.email}, 
-        process.env.JWT_secret,
+        process.env.JWT_SECRET,
         {expiresIn: '24h'}
     );
 }
